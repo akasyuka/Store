c49e45435c4c22cc498ed220fb9c546d2a94c7ad
akasyuka <aleksandrkasyuk@gmail.com>
2022-01-05 19:20:34 +0500

    Add navigation and refactoring templates

--- src/main/java/com/akasyuka/website/controllers/MainController.java
+++ src/main/java/com/akasyuka/website/controllers/MainController.java
@@ -7,10 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home (Model model) {
-        model.addAttribute("title", "Главная страница");
+        model.addAttribute("title", "Главная");
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about (Model model) {
+        model.addAttribute("title", "Главная");
+        return "about";
+    }
+
+    @GetMapping("/products")
+    public String products (Model model) {
+        model.addAttribute("title", "Главная");
+        return "products";
     }
 
 }
