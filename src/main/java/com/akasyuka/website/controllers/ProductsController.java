4c4225c8fbabbcc46b6f6ab33ea5c88fe26e1c89
akasyuka <aleksandrkasyuk@gmail.com>
2022-01-15 15:23:31 +0500

    Products Id page and controller

--- src/main/java/com/akasyuka/website/controllers/ProductsController.java
+++ src/main/java/com/akasyuka/website/controllers/ProductsController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class ProductsController {
@@ -34,6 +38,15 @@ public class ProductsController {
         Goods post = new Goods(place1, place2, place3);
         goodsRepository.save(post);
         return "redirect:/products";
+    }
+
+    @GetMapping("/products/{id}")
+    public String productDetails (@PathVariable(value = "id") long id, Model model) {
+        Optional<Goods> goods = goodsRepository.findById(id);
+        ArrayList<Goods> res = new ArrayList<>();
+        goods.ifPresent(res::add);
+        model.addAttribute("goods", res);
+        return "productsId";
     }
 
 }
