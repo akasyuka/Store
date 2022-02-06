cb0f02073a2b5c54c8cee5cd9555a735911ef5cb
akasyuka <aleksandrkasyuk@gmail.com>
2022-02-06 17:25:12 +0500

    AAA

--- src/main/java/com/akasyuka/website/repositories/OrderDetailsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.akasyuka.website.repositories;
-
-import com.akasyuka.website.models.OrderDetails;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OrderDetailsRepository extends CrudRepository<OrderDetails, Long> {
-}
