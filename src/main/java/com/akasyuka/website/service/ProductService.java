c5e80f06c24e4d085d52de98f253f0c88895fa2b
akasyuka <aleksandrkasyuk@gmail.com>
2022-02-22 09:39:50 +0300

    Refactoring

--- src/main/java/com/akasyuka/website/service/ProductService.java
+++ src/main/java/com/akasyuka/website/service/ProductService.java
@@ -2,7 +2,6 @@ package com.akasyuka.website.service;
 
 import com.akasyuka.website.entity.Product;
 
-
-public interface GoodsService {
-    Product findById(Long id);
+public interface ProductService {
+//    Product findById(Long id);
 }
